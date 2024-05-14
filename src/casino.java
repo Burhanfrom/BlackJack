09ab48e48d36c409ac3a5e624ef0505a71060f9a
bshahbaz25 <burhan_shahbaz25@milton.edu>
2024-05-14 08:59:27 -0400

    Initial commit

---
+++ src/casino.java
@@ -0,0 +1,8 @@
+public class casino {
+    public static void main(String[] args) {
+        casino x = new casino();
+    }
+    public casino(){
+        System.out.println("Welcome to Burhan's Casino");
+    }
+}
---
+++ src/player.java
@@ -0,0 +1,9 @@
+public class player {
+    public boolean isDealer;
+
+    public int cardssum;
+
+    public boolean isBust;
+
+    public Card[] hand ;
+}
