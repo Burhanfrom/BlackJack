96d1e952310aae73ba89b0357e969f9d0cb99044
bshahbaz25 <burhan_shahbaz25@milton.edu>
2024-05-17 10:30:20 -0400

    In the Card class, make a constructor for the Card

--- src/Card.java
+++ src/Card.java
@@ -1,7 +1,18 @@
 public class Card {
+
+    //public static void main(String[] args) {
+
 public String suit;
 
 public int value;
 
 public String name;
+
+public Card(String psuit,int pvalue, String pname){
+
+
+
+    System.out.println("the Ace of diamonds is worth 11 points");
+
+    }
 }
