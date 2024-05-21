1b48c0e9a1b21ce0cfcf5af63b3aea0b4c4749f6
bshahbaz25 <burhan_shahbaz25@milton.edu>
2024-05-21 10:41:54 -0400

    In the Card class, make a constructor for the Card

--- src/Card.java
+++ src/Card.java
@@ -8,11 +8,81 @@ public int value;
 
 public String name;
 
-public Card(String psuit,int pvalue, String pname){
+public Card(int pname,int psuit){
+//name=pname;
+//suit=psuit;
+
+    if(pname == 0){
+    value = 11;
+    name="Ace";
+}
+    if(pname == 1) {
+        value = 2;
+        name = "Two";
+    }
+        if(pname == 3) {
+            value = 4;
+            name = "three";
+        }
+    if(pname == 4) {
+        value = 5;
+        name = "four";
+    }
+    if(pname == 5) {
+        value = 6;
+        name = "five";
+    }
+    if(pname == 6) {
+        value = 7;
+        name = "six";
+    }
+    if(pname == 7) {
+        value = 8;
+        name = "seven";
+    }
+    if(pname == 8) {
+        value = 9;
+        name = "eight";
+    }
+    if(pname == 9) {
+        value = 10;
+        name = "nine";
+    }
+    if(pname == 10) {
+        value = 10;
+        name = "jack";
+    }
+    if(pname == 11) {
+        value = 10;
+        name = "QUEEN";
+    }
+    if(pname == 12) {
+        value = 10;
+        name = "King";
+    }
 
 
+        if(psuit == 0){
+         suit = "Hearts";
 
-    System.out.println("the Ace of diamonds is worth 11 points");
+        }
+    if(psuit == 1){
+        suit = "Diamonds";
 
+    }
+    if(psuit == 2){
+        suit = "Spades";
+
+    }
+    if(psuit == 3){
+        suit = "Clubs";
+
+    }
+
+  //  System.out.println("the Ace of diamonds is worth 11 points");
+
+    }
+    public void print(){
+    System.out.println("the" + name + "if" + suit + "is worth" +value+ "points");
     }
 }
--- src/casino.java
+++ src/casino.java
@@ -1,8 +1,32 @@
 public class casino {
+
+    public Card aceofHearts;
+    public Card[] Deck;
+
     public static void main(String[] args) {
         casino x = new casino();
     }
-    public casino(){
+
+    public casino() {
         System.out.println("Welcome to Burhan's Casino");
+        Deck = new Card [52];
+        int door = 0;
+        Deck[0] = aceofHearts;
+        for (int j = 0; j < 4; j++) {
+            for (int i = 0; i < 13; i++) {
+                aceofHearts = new Card(i, j);
+              //  System.out.println(aceofHearts);
+                aceofHearts.print();
+                Deck[door]=aceofHearts;
+                door ++;
+            }
+        }
+        printdeck();
+    }
+    public void printdeck(){
+        for(int i=0; i<Deck.length; i++){
+            Deck[i].print();
+            // Deck[i].print();
+        }
     }
 }
